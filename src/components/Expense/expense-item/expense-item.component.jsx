adf8b33e14a77bb05415c1fb71a39aba8a07e639
Debadrita438 <bristibandyopadhyay@gmail.com>
2021-08-20 11:00:32 +0530

    Filtering the array of expenses

--- src/components/Expense/expense-item/expense-item.component.jsx
+++ src/components/Expense/expense-item/expense-item.component.jsx
@@ -1,27 +1,18 @@
-import { useState } from 'react';
-
 import Card from '../../UI/card/card.component';
 import ExpenseDate from '../expense-date/expense-date.component';
 
 import './expense-item.styles.css';
 
-const ExpenseItem = ({ title, amount, date }) => {
-    const [name, setName] = useState(title);
-    const handleClick = () => {
-        setName('Updated');
-    }
+const ExpenseItem = ({ title, amount, date }) => (
+    <Card className='expense-item'>
+        <ExpenseDate date={date} />
+        <div className='expense-item__description'>
+            <h2>{title}</h2>
+            <div className='expense-item__price'>₹{amount}</div>
+        </div>
+    </Card>
+);
 
-    return (
-        <Card className='expense-item'>
-            <ExpenseDate date={date} />
-            <div className='expense-item__description'>
-                <h2>{name}</h2>
-                <div className='expense-item__price'>₹{amount}</div>
-            </div>
-            <button onClick={handleClick}>Change Title</button>
-        </Card>
-    );
-}
 
 
 export default ExpenseItem;
--- src/components/Expense/expenses/expenses.component.jsx
+++ src/components/Expense/expenses/expenses.component.jsx
@@ -1,17 +1,51 @@
+import { useState } from 'react';
 import Card from '../../UI/card/card.component';
+import ExpenseFilter from '../expense-filter/expense-filter.component';
 import ExpenseItem from '../expense-item/expense-item.component';
 
 import './expenses.styles.css';
 
-const DisplayExpense = ({ expenses }) => {
+const Expenses = ({ expenses }) => {
+    const [selectedYear, setSelectedYear] = useState('2019');
+    const selectYearHandler = selectedYear => {
+        setSelectedYear(selectedYear);
+    }
+
+    const filteredExpenses = expenses.filter(expense => {
+        return expense.date.getFullYear().toString() === selectedYear
+    });
+
+    let expensesContent = <p>No Expenses Found! :(</p>;
+
+    if(filteredExpenses.length > 0) {
+        expensesContent = filteredExpenses.map(expense =>
+            <ExpenseItem
+                key={expense.id}
+                title={expense.title}
+                amount={expense.amount}
+                date={expense.date}
+            />
+        )
+    }
+
     return (
         <Card className='expenses'>
-            <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} />
-            <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
-            <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} />
-            <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date} />
+            <ExpenseFilter onSelectedYear={selectYearHandler} year={selectedYear} />
+            {/* { filteredExpenses.length === 0 && <p>No Expenses Found! :(</p> }   When the first condition is approved then the statement beyond the && is shown to the browser
+            {
+                filteredExpenses.length > 0 && filteredExpenses
+                .map(expense =>
+                    <ExpenseItem
+                        key={expense.id}
+                        title={expense.title}
+                        amount={expense.amount}
+                        date={expense.date}
+                    />
+                )
+            } */}
+            {expensesContent}
         </Card>
     );
 }
 
-export default DisplayExpense;
+export default Expenses;
